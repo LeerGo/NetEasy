2d1a3f5b08fffb978fe121fe1ef6424ec0d9b734
alex.lee <liyifang>
2015-07-30 22:00:29 +0800

    更新私人FM歌词界面

--- app/src/main/java/me/xoder/neteasy/fragment/discover/fm/LyricFragment.java
+++ app/src/main/java/me/xoder/neteasy/fragment/discover/fm/LyricFragment.java
@@ -26,7 +26,7 @@ public class LyricFragment extends Fragment {
 	@Nullable
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_lyric, container, false);
+		View view = inflater.inflate(R.layout.fragment_fm_lyric, container, false);
 		initViews(view);
 		
 		return view;
